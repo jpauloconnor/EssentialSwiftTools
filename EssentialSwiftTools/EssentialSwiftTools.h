75704f0416b06af9b54e5b29035c4cb2ee68d062
Paul O'Connor <jamespauloconnor@gmail.com>
2015-09-29 11:46:20 -0400

    Initial Commit

---
+++ EssentialSwiftTools/EssentialSwiftTools.h
@@ -0,0 +1,19 @@
+//
+//  EssentialSwiftTools.h
+//  EssentialSwiftTools
+//
+//  Created by Paul O'Connor on 9/29/15.
+//  Copyright © 2015 OCApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EssentialSwiftTools.
+FOUNDATION_EXPORT double EssentialSwiftToolsVersionNumber;
+
+//! Project version string for EssentialSwiftTools.
+FOUNDATION_EXPORT const unsigned char EssentialSwiftToolsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EssentialSwiftTools/PublicHeader.h>
+
+
